bc9a1a54d137f2d1539cbfe3329bff59d9361909
Jayson Messenger <jayson@arrowbox.dev>
2020-03-06 12:39:46 -0500

    Add  plugin for git completion

--- .config/zsh/interactive/01_zplug.zsh
+++ .config/zsh/interactive/01_zplug.zsh
@@ -2,6 +2,7 @@ export ZPLUG_HOME=$XDG_DATA_HOME/zplug
 source $ZPLUG_HOME/init.zsh
 
 zplug 'mfaerevaag/wd', as:command, use:"wd.sh", hook-load:"wd() { . $ZPLUG_REPOS/mfaerevaag/wd/wd.sh }"
+zplug 'plugins/gitfast', from:oh-my-zsh
 
 # Install plugins if there are plugins that have not been installed
 if ! zplug check --verbose; then
